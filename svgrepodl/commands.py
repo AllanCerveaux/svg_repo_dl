afe1beb28c810a4e90cf4dbf11e6a54804f2e519
callan <cerveauxallanjean@gmail.com>
2020-07-18 12:03:42 +0200

    Add DocBlock

--- svgrepodl/commands.py
+++ svgrepodl/commands.py
@@ -5,4 +5,15 @@ from .utils import getUserDocumentPath, downloader
 @click.option('--path', '-p', default=getUserDocumentPath(), help="Change download destination path.")
 @click.argument('url')
 def cli(path, url):
+	"""Run Command
+	Run downloader to get SVGREPO pack
+	
+	Decorators:
+		click.command
+		click.option
+		click.argument
+	Arguments:
+		path {[string]} -- Destination download path
+		url {[string]} -- URL of SVGREPO Collection
+	"""
 	downloader(url, path)
