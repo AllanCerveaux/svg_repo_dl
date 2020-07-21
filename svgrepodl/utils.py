8be79f284d0ed959526a8c943af047b1c884845c
callan <cerveauxallanjean@gmail.com>
2020-07-21 11:50:18 +0200

    move all url method into Url.py

--- svgrepodl/utils.py
+++ svgrepodl/utils.py
@@ -1,5 +1,3 @@
-import re
-import sys
 import time
 import click
 from os.path import expanduser
@@ -14,17 +12,6 @@ def getUserDocumentPath():
 		[string] -- Return destanation download path
 	"""
 	return expanduser("~") + "/Documents/icons/"
-
-def collectionName(url):
-	"""Split URL and get collection name
-	Arguments:
-		url {[string]} -- URL of SVGREPO Collection
-	Returns:
-		[string] -- Collection Name
-	"""
-	url_splited = url.split('/')
-	url_splited = [i for i in url_splited if i]
-	return url_splited.pop()
 
 def downloader(url, path):
 	urlChecker(url)
@@ -51,20 +38,6 @@ def browserConfiguration(path):
 	options = Options()
 	options.add_argument("--headless")
 	return webdriver.Firefox(firefox_profile=profile, options=options)
-
-def urlChecker(url):
-	"""Check url
-	Regex check if given by user equal to svgrepo collection
-	
-	Arguments:
-		url {[type]} -- [description]
-	"""
-	pattern =  '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]svgrepo+)\.[a-z]{2,5}(\/collection\/)([a-zA-Z0-9-_]+)/?$'
-	result = re.match(pattern,url)
-	if not result:
-		click.echo('😱 Oups URL provided not match !')
-		click.echo('💡 Your URL should look like this : https://svgrepo/collection/[id]')
-		sys.exit()
 
 # @TODO=use WebDriverWait and find_elements_by_*
 def runBrowser(driver, url):
