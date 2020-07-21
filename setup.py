dd9eafc52144caaffb6d424a6151c78de96b63cd
callan <cerveauxallanjean@gmail.com>
2020-07-21 11:08:34 +0200

    add more information in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
 	name = 'svgrepodl',
 	author="callan",
 	author_email="cerveauxallanjean@gmail.com",
 	description="Pack Downloader for SVG REPO",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
 	homepage="https://github.com/AllanCerveaux/svg_repo_dl/blob/master/README.md",
 	version = '0.0.1',
 	license="MIT",
@@ -13,6 +18,12 @@ setup(
 		'selenium',
 		'progress',
 		'click',
+		'colored',
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
 	],
 	entry_points = {
 		'console_scripts': [
